87c62875b78a0f32eb737622cf8bab0587d5f109
Alexandre <alexandre.clain11@gmail.com>
2020-09-15 16:15:48 +0200

    build

---
+++ client/build/precache-manifest.b2d727a01587cd75a4794dea82a1f9fe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe253c1478e79b16f5d11ae7ccb6f44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98abb3004c8408a69598",
+    "url": "/static/css/main.cf00d1ca.chunk.css"
+  },
+  {
+    "revision": "b66f33f243641deb10f4",
+    "url": "/static/js/2.05b1fda8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.05b1fda8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98abb3004c8408a69598",
+    "url": "/static/js/main.3e67fe60.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "57cf6c5c7de1387a26e73467a47c1f62",
+    "url": "/static/media/ReenieBeanie-Regular.57cf6c5c.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "9e06bf8e4155ad3a942a9ff38f59fbc4",
+    "url": "/static/media/Roboto-Mono.9e06bf8e.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/static/media/Roboto-Thin.321de678.ttf"
+  }
+]);
